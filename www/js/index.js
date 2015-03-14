14c13ff1347ea9f72eece80ba0335bbb2114558a
Lt-Mayonesa <juacocampero@gmail.com>
2015-03-14 17:29:04 -0300

    app navigation

--- www/js/index.js
+++ www/js/index.js
@@ -1,10 +1,75 @@
+var navLog = [];
+
 //page obj
-var page = {
-    'current' : {
+var $page = {
+    current : '',
+    header : '',
+    content : '',
+    footer : '',
+    /**
+     * 
+     * @param {string} page asign current page elementes
+     * @returns {void}
+     */
+    buildPage : function (page) {
+        this.header = page + ' .doc-header';
+        this.content = page + ' .doc-content';
+        this.footer = page + ' .doc-footer';
+    },
+    /**
+    * 
+    * @param {string} id the id of the page to navigate to
+    * @param {string} animation tween, fade and that stuff
+    * @returns {void}
+    */
+    toPage : function (id, animation, back) {
+        if ($page.current !== '') {
+            $($page.current).hide();
+            var navObj = {
+                pageId : $page.current,
+                tween : animation
+            };
+            if(!back) navLog.push(navObj);
+        }
+        $page.current = id;
+        $page.buildPage($page.current);
+        $($page.current).show();
+        console.log(navLog);
+        if ($page.current === '#home') navLog = [];
+    },
+    
+    goBack : function () {
+        if((navLog.length - 1) === -1) {
+            navigator.notification.confirm(
+                i18n.t('dialogs.exitDialogMsg'),
+                function (index) {
+                    if (index === 1) navigator.app.exitApp();
+                    else return false;
+                },
+                i18n.t('dialogs.exitDialogTitle'),
+                [i18n.t('dialogs.btnYes'),i18n.t('dialogs.btnNo')]
+            );
+        } else {
+            var backTo = navLog[(navLog.length) - 1];
+            navLog.pop();
+            console.log(navLog);
+            this.toPage(backTo.pageId, backTo.tween, true);
+        }
         
     }
 };
 
+
+//---------------------------------------------------------------------
+
+/**
+ * main function after laguage has been loaded
+ * @returns void
+ */
+function init() {
+    $page.toPage('#home', '');
+    
+}
 
 /**
  * set language based on device lang
@@ -18,17 +83,27 @@ function setLanguage() {
         // some timeout for fake loading... lel
         setTimeout(function() {
             $('#start-page').hide();
-            getUserExams();
-        }, 1000);
+            init();
+        }, 2000);
   });
 }
 /**
  * get list of user exams stored on device
  */
 function getUserExams() {
-    window.plugins.spinnerDialog.show(null, 'mensajito');
+    
 }
-
+/**
+ * creates json file exam and store localy;
+ * @param {type} form the form submited $('bla')
+ * @returns {undefined} null
+ */
+function createExamQA(form) {
+    
+    newTitle = newTitle.replace(/[^a-zA-Z0-9\s]/g,"");
+    newTitle = newTitle.toLowerCase();
+    newTitle = newTitle.replace(/\s/g,'-');
+}
 var app = {
     // Application Constructor
     initialize: function() {
@@ -40,6 +115,10 @@ var app = {
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
+        document.addEventListener('backbutton', function (e) {
+            e.preventDefault;
+            $page.goBack();
+        }, false);
     },
     // deviceready Event Handler
     //
